cfdc894d2d83ce10798b9e65a36ba84314de05f5
Emmanuel <emmanuel.nuno.estrella@uabc.edu.mx>
2020-05-02 13:18:35 -0700

    encuesta de satisfaccion
    encuesta de satisfaccion funcionando

--- resources/html/encuestas.php
+++ resources/html/encuestas.php
@@ -40,50 +40,55 @@ session_start();
 	
 	
     <div class="contenido">
+
+
         <!-- Barra superior-->
         <section>
             <div class="top-barr"></div>
         </section>
 
         <div>
-        <h2>¿Como te atendimos hoy?</h2>
+             <h2>¿Cómo te atendimos hoy?</h2>
         </div>
            
         <!-- Emojis para encuesta de satisfaccion -->
         <section class="seccion-botones satisfaccion">  
                 <div class="botones-child">
-                    <a href=""id="excelent" class="icon excelent" data-value="5" onclick="myFunction();" ></a>
+                    <button id="excelent" class="icon excelent" data-value="5" onclick="myFunction('excelent');" ></button>
                 </div>
                 <div class="botones-child" >
-                <a href="" id="good"class="icon good" data-value="4" onclick="myFunction();" ></a>
+                <button id="good"class="icon good" data-value="4" onclick="myFunction('good');" ></button>
                 </div>
                 <div class="botones-child">
-                <a href="" id="fine"class="icon fine" data-value="3" onclick="myFunction();"></a>
+                <button id="fine"class="icon fine" data-value="3" onclick="myFunction('fine');"></button>
                 </div>
                 <div class="botones-child">
-                <a href="" id="well" class="icon well" data-value="2" onclick="myFunction();"></a>
+                <button id="well" class="icon well" data-value="2" onclick="myFunction('well');"></button>
                 </div>
                 <div class="botones-child">
-                <a href="" id="bad" class="icon bad" data-value="1" onclick="myFunction();"></a>
+                <button id="bad" class="icon bad" data-value="1" onclick="myFunction('bad');"></button>
                 </div>   
 
         </section>
 
         <script>
-
-            var input;
-
-   
-            function myFunction()
+            function myFunction(voto)
             {
-             var json 
-            }
-       
+                var voto = voto
+                console.log(voto)
+                var valorVoto = document.getElementById(voto).getAttribute("data-value");
+                console.log(valorVoto)
+
+               }
         </script>
+
+
     </div>    
 </body>
-<!-- Barra inferior-->
-<footer class="footer">
 
-</footer>
+
+<!-- Barra inferior-->
+<footer class="footer"></footer>
+
+
 </html>
