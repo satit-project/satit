e5d56ea54aae6f4157e7c3d858247fc238ed57ac
oscarAMS95 <32280104+oscarAMS95@users.noreply.github.com>
2020-07-15 22:22:04 -0700

    Pagina de gestion de nomina Administrador

---
+++ nominaAdmin/dbConfig.php
@@ -0,0 +1,13 @@
+<?php
+    $dbHost = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "satit";
+
+    $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    if($db->connect_error)
+    {
+        die("Connection failed: " . $db->connect_error);
+    }
+
+    ?>
---
+++ nominaAdmin/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>  
+<html>  
+    <head>  
+        <title>Nomina Admin</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    </head>  
+    <body>  
+        <?php
+            include_once 'dbConfig.php';
+            if(!empty($_GET['status']))
+            {
+                switch($_GET['status'])
+                {
+                    case 'succ':
+                        $statusType = 'alert-success';
+                        $statusMsg = "Se han importado los datos satisfactoriamente";
+                    break;
+                    case 'err':
+                        $statusType = 'alert-danger';
+                        $statusMsg = 'Ocurrio un error';
+                    break;
+                    default:
+                        $statusType = '';
+                        $statusMsg = '';
+                }
+            }
+        ?>
+
+        <?php
+            if(!empty($statusMsg))
+            {?>
+                <div class="col-xs-12">
+                    <div class="alert <?php echo $statusType; ?>"><?php echo $statusMsg; ?></div>
+                </div>
+            <?php }
+        ?>
+
+        <div class="row">
+            <div class="col-md-12 head">
+                <div class="float-right">
+                    <a href="javascript:void(0);" class="btn btn-success" onclick="formToggle('importFrm');"><i class="plus"></i>Importar datos</a>
+                </div>
+            </div>
+            <div class="col-md-12" id="importFrm" style="display: none;">
+                <form action="importDB.php" method="post" enctype="multipart/form-data">
+                    <input type="file" name="file"/>
+                    <input type="submit" class="btn btn-primary" name="importSubmit" value="IMPORT">
+                </form>
+            </div>
+            <table class="table table-striped table-bordered">
+                <thead class="thead-dark">
+                    <tr>
+                        <th class="text-center">#ID Registro</th>
+                        <th class="text-center">Horas</th>
+                        <th class="text-center">Pago</th>
+                        <th class="text-center">Bono</th>
+                        <th class="text-center">ID empleado</th>
+                        <th class="text-center">Fecha</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                // Get member rows
+                $result = $db->query("SELECT * FROM nomina");
+                if($result->num_rows > 0){
+                    while($row = $result->fetch_assoc()){
+                ?>
+                    <tr>
+                        <td class="text-center"><?php echo $row['id']; ?></td>
+                        <td class="text-center"><?php echo $row['horas'];?></td>
+                        <td class="text-center"><?php echo $row['pago'];?></td>
+                        <td class="text-center"><?php echo $row['bono'];?></td>
+                        <td class="text-center"><?php echo $row['empleado_id'];?></td>
+                        <td class="text-center"><?php echo $row['fecha'];?></td>
+                    </tr>
+                <?php } }else{ ?>
+                    <tr><td colspan="5">No member(s) found...</td></tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        </div>
+        <script>
+            function formToggle(ID)
+            {
+                var element = document.getElementById(ID);
+                if(element.style.display === "none")
+                {
+                    element.style.display = "block";
+                }
+                else
+                {
+                    element.style.display = "none";
+                }
+            }
+        </script>
+    </body>  
+</html>
