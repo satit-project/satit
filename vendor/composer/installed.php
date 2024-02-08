c6e8bfcbc26b910e75554fe677525c0924292b42
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2024-02-08 14:07:54 -0800

    added composer.lock details

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'satit/satit',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f15949cd87a4e583865847066cc033367d11aa32',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'satit/satit' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f15949cd87a4e583865847066cc033367d11aa32',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
