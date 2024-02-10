4c0a90724715fba5ac5cb6f1465baee394b41256
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2024-02-09 16:37:00 -0800

    removed unnecessary info

--- resources/php/conn.php
+++ resources/php/conn.php
@@ -1,7 +1,7 @@
 <?php
 // Connection variables
-$dbhost	= "localhost";	   // localhost or IP
-$dbuser	= "root";		  // database username
-$dbpass	= "SurfC0d32ndL0v3L1f3!";		     // database password
-$dbname	= "u734165210_satit";    // nombre de la base de datos que cree en phpmyadmin de xampp
+$dbhost	= "";	   // localhost or IP
+$dbuser	= "";		  // database username
+$dbpass	= "";		     // database password
+$dbname	= "";    // nombre de la base de datos que cree en phpmyadmin de xampp
 ?>	
